204b0525f367b1d08ec1f36222d016f831b0506d
Nikita Popov <mailto.nikitapopov@gmail.com>
2021-01-28 00:41:25 +0300

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# CrowdHuman for YOLO
+
+[CrowdHuman](http://www.crowdhuman.org/download.html) dataset prepared for [darknet](https://github.com/AlexeyAB/darknet) implementation
+
+Optional annotations were ignored, only visible boxes are used.
+All annotations are standartized to `<object-class> <x> <y> <width> <height>`, where:
+
+- `<object-class>` - string class of object from (person or mask)
+- `<x> <y> <width> <height>` - float values relative to width and height of image, it can be set from 0.0 to 1.0
+- for example: `<x> = <absolute_x> / <image_width>` or `<height> = <absolute_height> / <image_height>`
+- **attention**: `<x> <y>` - is center of rectangle (not top left corner)
+
+## Details
+
+Annotation files are included in `/annotations` folder.
+Images are not part of this repo, please download it by yourself and put to matching folders:
+`/images_{suffix}`, where `{suffix}` is `test`, `train` or `val`
+
+Algorithm creates `.txt` annotation file for each image (matching ID in each annotation row)
+Use `python3 main.py` to run script
+
+### Requirements
+
+Use `pip3 install -r requirements.txt` to install python dependencies
---
+++ main.py
@@ -0,0 +1,50 @@
+import json
+import os
+import pandas as pd
+from pprint import pprint
+from PIL import Image
+
+cwd = os.getcwd()
+
+def generate_annotations(filename):
+    with open(f"{cwd}/annotations/{filename}", 'r') as file:
+        suffix = filename.split('.')[0].split('_')[1]
+
+        data = file.read()
+        lines = data.split('\n')
+        annotations = []
+        for line in lines:
+            try:
+                annotations.append(json.loads(line))
+            except:
+                pass
+
+        images = []
+        for annotation in annotations:
+            image = {}
+            image['id'] = annotation['ID']
+
+            picture = Image.open(f"{cwd}/images_{suffix}/{image['id']}.jpg")
+            width, height = picture.size
+            image['annotations'] = [
+                {
+                    'tag': box['tag'],
+                    'x': float(box['vbox'][0]) / float(width),
+                    'y': float(box['vbox'][1]) / float(height),
+                    'w': float(box['vbox'][2]) / float(width),
+                    'h': float(box['vbox'][3]) / float(height)
+                } for box in annotation['gtboxes']]
+            images.append(image)
+            with open(f"{cwd}/results/{image['id']}.txt", 'w') as output:
+                output.writelines([f"{line['tag']} {line['x']} {line['y']} {line['w']} {line['h']}\n" for line in image['annotations']])
+
+
+if __name__ == '__main__':
+    list = os.listdir(f"{cwd}/annotations/")
+    try:
+        list.remove('.DS_Store')  # for macOS
+    except:
+        pass
+    for filename in list:
+        print(filename)
+        generate_annotations(filename)
